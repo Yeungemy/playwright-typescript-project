ad90fca0aa7625e42f6192371f0cff016376e5e3
emy.yeung <emy.yeung@computerfworkware.com>
2025-05-22 20:08:37 -0400

    restored

--- src/tests/e2e/specs/login.spec.ts
+++ src/tests/e2e/specs/login.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from '../pages/pageFactory';
 
 test.describe('Login Page', () => {
 
-  test.beforeEach(async () => {
+  test.beforeEach(async ({ page }) => {
     console.log(process.env.BASE_URL);
-    
+    await page.goto(process.env.BASE_URL, { timeout: 60000 });
   });
 
-  test('@smoke should display the member portal link', async ({ page }) => {
-    await page.goto(process.env.BASE_URL, { timeout: 60000 });
+  test('@smoke should display the member portal link', async ({ loginPage }) => {
+    await expect(loginPage.SELECTORS.memberPortalLink).toBeVisible();
   });
 
   test('@sanity should display the employer portal link', async ({ loginPage }) => {
